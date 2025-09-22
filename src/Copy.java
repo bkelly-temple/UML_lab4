b6c4602c28e66a8856d9085a3313aa3455bc147e
bkelly-temple <136024320+bkelly-temple@users.noreply.github.com>
2025-09-22 18:35:46 -0400

    Update Copy.java

--- src/Copy.java
+++ src/Copy.java
@@ -1,3 +1,19 @@
 public class Copy {
+String coypId;
+    String condition;
+    boolean isAvailable;
+    Book book;  
 
+    public boolean checkAvailability() {
+        // Implementation to check availability
+        return isAvailable;
+    }public void updateCondition(String condition) {
+        // Implementation to update condition
+        this.condition = condition;
+    }
+    public List<Book> getBook() {
+        // Implementation to get book details
+        return new ArrayList<>();
+    }
 }
+
