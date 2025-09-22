0680d38941f7af72f54fae4021d4313c9ac84002
bkelly-temple <136024320+bkelly-temple@users.noreply.github.com>
2025-09-22 18:50:10 -0400

    Update loan.java

--- src/loan.java
+++ src/loan.java
@@ -1,3 +1,27 @@
 public class loan {
+String loanId;
+    Date borrowDate;
+    Date dueDate;
+    Date returnDate;
+    Double fineAmount;
+    Member member;
+    Copy copy;
+    Librarian issueLibrarian;
 
+    public boolean isOverdue() {
+        // Implementation to check if loan is overdue
+        return false;
+    }
+    public Double calculateFine() {
+        // Implementation to calculate fine
+        return 0.0;
+    }
+    public void markReturned() {
+        // Implementation to mark loan as returned
+    }
+    public boolean renew() {
+        // Implementation to renew loan
+        return true;
+    }
 }
+
