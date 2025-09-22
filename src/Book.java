9b81d016c02bfc5ae301f2316c1622ba728801fb
bkelly-temple <136024320+bkelly-temple@users.noreply.github.com>
2025-09-22 18:33:45 -0400

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,23 @@
 public class Book {
+ String isbn;
+    String title;
+    int publicationYear;
+    List<Author> authors;
+    List<Copy> copies; 
 
+    public String getSummary() {
+        // Implementation to get book summary
+        return "";
+    }
+    public void addAuthor(Author author) {
+        // Implementation to add author
+    }
+    public void addCopy(Copy copy) {
+        // Implementation to add copy
+    }   
+    public List<Copy> getAvailableCopies() {
+        // Implementation to get available copies
+        return new ArrayList<>();
+    }
 }
+
