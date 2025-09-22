b59161727a4e3f6638fb285db82f0703359ac540
bkelly-temple <136024320+bkelly-temple@users.noreply.github.com>
2025-09-22 18:36:52 -0400

    Update Librarian.java

--- src/Librarian.java
+++ src/Librarian.java
@@ -1,3 +1,33 @@
-public class Librarian {
+class Librarian extends User {
+    Date hireDate;
+    String employeeId;
+    private Library library;
+
+    public void addBookToCatalog(Book book, int copies) {
+        // Implementation to add book to catalog
+    }
+    public void removeBookFromCatalog(Book book) {
+        // Implementation to remove book from catalog
+    }
+    public void registerNewMember(Member member) {
+        // Implementation to register new member
+    }
+    public List<Loan> issueLoan(Member member, Copy copy) {
+        // Implementation to issue loan
+        return new ArrayList<>();
+    }
+    public void processReturn(Loan loan) {
+        // Implementation to process return
+    }
+    public List<Loan> generateOverdueReport() {
+        // Implementation to generate overdue report
+        return new ArrayList<>();
+    }
+    public boolean renewLoan(Loan loan) {
+        // Implementation to renew loan
+        return true;
+    }
+
 
 }
+
