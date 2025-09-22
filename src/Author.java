258fb3f6d1617b02bd4caabfaadce24b74b6becc
bkelly-temple <136024320+bkelly-temple@users.noreply.github.com>
2025-09-22 18:29:09 -0400

    Update Author.java

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,18 @@
 public class User {
+ private String userId;
+    private String name;
+    private String email;
+    private String password;
 
+    public boolean login(String email, String password) {
+        // Implementation for user login
+        return true;
+    }
+    public void logout(){
+        // Implementation for user logout
+    }
+    public void updateProfile(String name, String email, String password) {
+        // Implementation to update user profile
+    }
 }
+
