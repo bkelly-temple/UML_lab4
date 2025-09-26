701349ca36afe15186f86d8685e5462f2e9e1f08
bkelly-temple <136024320+bkelly-temple@users.noreply.github.com>
2025-09-26 10:00:01 -0400

    Update Author.java
    added setters/getters fixed inheritance

--- src/Author.java
+++ src/Author.java
@@ -1,18 +1,33 @@
-public class User {
- private String userId;
-    private String name;
-    private String email;
-    private String password;
+import java.util.*;
 
-    public boolean login(String email, String password) {
-        // Implementation for user login
-        return true;
+public class Author {
+    private String name;
+    private String bio;
+    private List<Book> books;
+    
+    // Constructor
+    public Author(String name, String bio) {
+        this.name = name;
+        this.bio = bio;
+        this.books = new ArrayList<>();
     }
-    public void logout(){
-        // Implementation for user logout
+
+    public void addBook(Book book) {
+        // TODO: Implementation to add book
+        if (!books.contains(book)) {
+            books.add(book);
+        }
     }
-    public void updateProfile(String name, String email, String password) {
-        // Implementation to update user profile
+    
+    public List<Book> getBibliography() {
+        // TODO: Implementation to get author's books
+        return new ArrayList<>(books);
     }
+    
+    // Getters
+    public String getName() { return name; }
+    public String getBio() { return bio; }
+    public List<Book> getBooks() { return books; }
 }
+
 
