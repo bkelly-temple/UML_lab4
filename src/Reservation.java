0e26892535468bcd50e053165525377dfd8efdf4
bkelly-temple <136024320+bkelly-temple@users.noreply.github.com>
2025-09-26 09:59:21 -0400

    Update Reservation.java
    added setter/getters fixed inheritance

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,25 +1,51 @@
+import java.util.*;
+
 public class Reservation {
-    String reservationId;
-    Date requestDate;
-    Date expiryDate;
-    Member member;
-    Book book;
-    int position;
-
-    public boolean isExpired() {
-        // Implementation to check if reservation is expired
-        return true;
+    private String reservationId;
+    private Date requestDate;
+    private Date expiryDate;
+    private int position;
+    private Member member;
+    private Book book;
+    
+    // Constructor
+    public Reservation(String reservationId, Date requestDate, Date expiryDate, int position, Member member, Book book) {
+        this.reservationId = reservationId;
+        this.requestDate = requestDate;
+        this.expiryDate = expiryDate;
+        this.position = position;
+        this.member = member;
+        this.book = book;
     }
+
     public void cancel() {
-        // Implementation to cancel reservation
+        // TODO: Implementation to cancel reservation
     }
+    
     public String getStatus() {
-        // Implementation to get reservation status
-        return "";
-    } 
-    public void notifyMember() {
-        // Implementation to notify member
+        // TODO: Implementation to get reservation status
+        if (isExpired()) {
+            return "Expired";
+        }
+        return "Active";
     }
-
+    
+    public boolean isExpired() {
+        // TODO: Implementation to check if reservation is expired
+        return new Date().after(expiryDate);
+    }
+    
+    public void notifyMember() {
+        // TODO: Implementation to notify member
+    }
+    
+    // Getters
+    public String getReservationId() { return reservationId; }
+    public Date getRequestDate() { return requestDate; }
+    public Date getExpiryDate() { return expiryDate; }
+    public int getPosition() { return position; }
+    public Member getMember() { return member; }
+    public Book getBook() { return book; }
 }
+
 
