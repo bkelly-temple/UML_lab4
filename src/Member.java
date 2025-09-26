4316f29875a58069b02d3681fc7a1a2eb7c2a9ac
bkelly-temple <136024320+bkelly-temple@users.noreply.github.com>
2025-09-26 09:53:56 -0400

    Update Member.java
    added setters/getters

--- src/Member.java
+++ src/Member.java
@@ -1,33 +1,63 @@
-public class Member extends User {
-    Date membershipDate;
-    int loanLimit;
-    List<Loan> activeLoans;
-    List<Reservation> reservations;
-    private Library library;
+import java.util.*;
 
-    public List<Loan> borrowCopy(Copy copy) {
-        // Implementation to borrow a copy
-        return new ArrayList<>();
+public class Member extends User {
+    private Date membershipDate;
+    private int loanLimit;
+    private List<Loan> activeLoans;
+    private List<Reservation> reservations;
+    private Library library;
+    
+    // Constructor
+    public Member(String userId, String name, String email, String password, Date membershipDate, int loanLimit) {
+        super(userId, name, email, password);
+        this.membershipDate = membershipDate;
+        this.loanLimit = loanLimit;
+        this.activeLoans = new ArrayList<>();
+        this.reservations = new ArrayList<>();
     }
+
+    public Loan borrowCopy(Copy copy) {
+        // TODO: Implementation to borrow a copy
+        return null;
+    }
+    
     public void returnLoan(Loan loan) {
-        // Implementation to return a copy
+        // TODO: Implementation to return a copy
+        if (activeLoans.contains(loan)) {
+            activeLoans.remove(loan);
+        }
     }
-    public List<Reservation> reserveBook(Book book) {
-        // Implementation to reserve a book
-        return new ArrayList<>();
+    
+    public Reservation reserveBook(Book book) {
+        // TODO: Implementation to reserve a book
+        return null;
     }
+    
     public void cancelReservation(Reservation reservation) {
-        // Implementation to cancel a reservation
+        // TODO: Implementation to cancel a reservation
+        if (reservations.contains(reservation)) {
+            reservations.remove(reservation);
+        }
     }
+    
     public List<Loan> getActiveLoans() {
         return activeLoans;
     }
+    
     public List<Reservation> getReservations() {
         return reservations;
     }
+    
     public List<Loan> getBorrowingHistory() {
-        // Implementation to get history
+        // TODO: Implementation to get history
         return new ArrayList<>();
     }
+    
+    // Getters
+    public Date getMembershipDate() { return membershipDate; }
+    public int getLoanLimit() { return loanLimit; }
+    public Library getLibrary() { return library; }
+    public void setLibrary(Library library) { this.library = library; }
 }
+
 
