494c30ade61c873a9ff9fd2d19b3793c7bebf8bf
bkelly-temple <136024320+bkelly-temple@users.noreply.github.com>
2025-09-26 09:56:55 -0400

    Update Book.java
    added setter/getters fixed inheritance

--- src/Book.java
+++ src/Book.java
@@ -1,23 +1,58 @@
+import java.util.*;
+
 public class Book {
- String isbn;
-    String title;
-    int publicationYear;
-    List<Author> authors;
-    List<Copy> copies; 
+    private String isbn;
+    private String title;
+    private int publicationYear;
+    private List<Author> authors;
+    private List<Copy> copies;
+    
+    // Constructor
+    public Book(String isbn, String title, int publicationYear) {
+        this.isbn = isbn;
+        this.title = title;
+        this.publicationYear = publicationYear;
+        this.authors = new ArrayList<>();
+        this.copies = new ArrayList<>();
+    }
 
     public String getSummary() {
-        // Implementation to get book summary
-        return "";
+        // TODO: Implementation to get book summary
+        return "Title: " + title + ", Year: " + publicationYear + ", ISBN: " + isbn;
     }
+    
     public void addAuthor(Author author) {
-        // Implementation to add author
+        // TODO: Implementation to add author
+        if (!authors.contains(author)) {
+            authors.add(author);
+            author.addBook(this);
+        }
     }
+    
     public void addCopy(Copy copy) {
-        // Implementation to add copy
+        // TODO: Implementation to add copy
+        if (!copies.contains(copy)) {
+            copies.add(copy);
+        }
     }   
+    
     public List<Copy> getAvailableCopies() {
-        // Implementation to get available copies
-        return new ArrayList<>();
+        // TODO: Implementation to get available copies
+        List<Copy> availableCopies = new ArrayList<>();
+        for (Copy copy : copies) {
+            if (copy.checkAvailability()) {
+                availableCopies.add(copy);
+            }
+        }
+        return availableCopies;
     }
+    
+    // Getters
+    public String getIsbn() { return isbn; }
+    public String getTitle() { return title; }
+    public int getPublicationYear() { return publicationYear; }
+    public List<Author> getAuthors() { return authors; }
+    public List<Copy> getCopies() { return copies; }
 }
+
 
