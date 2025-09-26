2975daeecdba90b345040aa5dc3b135299cfd5bf
bkelly-temple <136024320+bkelly-temple@users.noreply.github.com>
2025-09-26 09:56:01 -0400

    Update Library.java
    added setters/getter fixed inheritance

--- src/Library.java
+++ src/Library.java
@@ -1,11 +1,14 @@
-public class Library {
-    String name;
-    String address;
-    ArrayList<Books> books;
-    ArrayList<Librarians> librarians;
-    ArrayList<Members> members;
+import java.util.*;
 
-     public Library(String name, String address) {
+public class Library {
+    private String name;
+    private String address;
+    private List<Book> books;
+    private List<Member> members;
+    private List<Librarian> librarians;
+    
+    // Constructor
+    public Library(String name, String address) {
         this.name = name;
         this.address = address;
         this.books = new ArrayList<>();
@@ -13,13 +16,67 @@ public class Library {
         this.librarians = new ArrayList<>();
     }
 
+    public void addBook(Book book, int copies) {
+        // TODO: Implementation to add book copies
+        if (!books.contains(book)) {
+            books.add(book);
+        }
+        // Add the specified number of copies to the book
+        for (int i = 0; i < copies; i++) {
+            Copy copy = new Copy("COPY_" + book.getIsbn() + "_" + System.currentTimeMillis(), "Good", book);
+            book.addCopy(copy);
+        }
+    }
+    
+    public void removeBook(Book book) {
+        // TODO: Implementation to remove book
+        books.remove(book);
+    }
 
-    public void addBook(Book book,int copies){
-         // add book to the list of books
-         for(int i=0;i<copies;i++){
-            books.add(book);// ad copies amount of books to the list
-         }
-         
+    public void registerMember(Member member) {
+        // TODO: Implementation to register member
+        if (!members.contains(member)) {
+            members.add(member);
+            member.setLibrary(this);
+        }
+    }
+    
+    public void removeMember(Member member) {
+        // TODO: Implementation to remove member
+        members.remove(member);
+    }
+    
+    public List<Book> findBookByTitle(String title) {
+        // TODO: Implementation to find books by title
+        List<Book> foundBooks = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getTitle().toLowerCase().contains(title.toLowerCase())) {
+                foundBooks.add(book);
+            }
+        }
+        return foundBooks;
+    }
+    
+    public void hireLibrarian(Librarian librarian) {
+        // TODO: Implementation to hire librarian
+        if (!librarians.contains(librarian)) {
+            librarians.add(librarian);
+            librarian.setLibrary(this);
+        }
+    }
+    
+    public List<Copy> getAvailableCopies(Book book) {
+        // TODO: Implementation to get available copies of a book
+        return book.getAvailableCopies();
+    }
+    
+    // Getters
+    public String getName() { return name; }
+    public String getAddress() { return address; }
+    public List<Book> getBooks() { return books; }
+    public List<Member> getMembers() { return members; }
+    public List<Librarian> getLibrarians() { return librarians; }
+}
 
 
 
@@ -50,3 +107,4 @@ public class Library {
             return list;
     
 }
+
