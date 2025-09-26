15bcc8a365ebd45667322e8d139dd6976d0de20f
bkelly-temple <136024320+bkelly-temple@users.noreply.github.com>
2025-09-26 09:54:44 -0400

    Update Librarian.java
    added setters/getters and fixed inheritance

--- src/Librarian.java
+++ src/Librarian.java
@@ -1,34 +1,61 @@
+import java.util.*;
+
 public class Librarian extends User {
-    Date hireDate;
-    String employeeId;
+    private Date hireDate;
+    private String employeeId;
     private Library library;
+    
+    // Constructor
+    public Librarian(String userId, String name, String email, String password, Date hireDate, String employeeId) {
+        super(userId, name, email, password);
+        this.hireDate = hireDate;
+        this.employeeId = employeeId;
+    }
 
     public void addBookToCatalog(Book book, int copies) {
-        // Implementation to add book to catalog
+        // TODO: Implementation to add book to catalog
+        if (library != null) {
+            library.addBook(book, copies);
+        }
     }
+    
     public void removeBookFromCatalog(Book book) {
-        // Implementation to remove book from catalog
+        // TODO: Implementation to remove book from catalog
+        if (library != null) {
+            library.removeBook(book);
+        }
     }
+    
     public void registerNewMember(Member member) {
-        // Implementation to register new member
+        // TODO: Implementation to register new member
+        if (library != null) {
+            library.registerMember(member);
+        }
     }
-    public List<Loan> issueLoan(Member member, Copy copy) {
-        // Implementation to issue loan
-        return new ArrayList<>();
+    
+    public Loan issueLoan(Copy copy, Member member) {
+        // TODO: Implementation to issue loan
+        return null;
     }
+    
     public void processReturn(Loan loan) {
-        // Implementation to process return
+        // TODO: Implementation to process return
     }
-    public List<Loan> generateOverdueReport() {
-        // Implementation to generate overdue report
+    
+    public List<Member> generateOverdueReport() {
+        // TODO: Implementation to generate overdue report
         return new ArrayList<>();
     }
+    
     public boolean renewLoan(Loan loan) {
-        // Implementation to renew loan
-        return true;
+        // TODO: Implementation to renew loan
+        return false;
     }
-
-
+    
+    // Getters
+    public Date getHireDate() { return hireDate; }
+    public String getEmployeeId() { return employeeId; }
+    public Library getLibrary() { return library; }
+    public void setLibrary(Library library) { this.library = library; }
 }
-
 
